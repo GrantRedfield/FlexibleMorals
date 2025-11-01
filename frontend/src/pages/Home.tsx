666a50e44384c70bb2028864b1e657b3e8d28a27
GrantRedfield <grant.a.redfield@gmail.com>
2025-11-01 12:15:19 -0500

    yay

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -37,7 +37,7 @@ export default function Home() {
 
   return (
     <div className="home-root">
-      {/* ✅ The real background layer */}
+      {/* ✅ Background layer (fills width & shows full image) */}
       <img
         src="/FlexibleMoralsPicture.png"
         alt="Flexible Morals Background"
